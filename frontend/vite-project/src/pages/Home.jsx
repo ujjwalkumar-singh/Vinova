66d7c25db0c692ecefa04b6a0d90143b7d714cb4
ujjwalkumar-singh <ujwsingh@gmail.com>
2026-03-18 23:54:44 +0530

    resoved imported file name

--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import withAuth from '../utils/withAuth'
+import withAuth from '../utils/WithAuth.jsx'
 import { useNavigate } from 'react-router-dom'
 import "../App.css";
 import { Button, IconButton, TextField } from '@mui/material';
